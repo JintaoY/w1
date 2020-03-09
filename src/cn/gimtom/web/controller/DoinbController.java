ec7d4a667b1c7f6f42293eb494961176f41f0aea
gjmtom <1596475940@qq.com>
2020-03-09 15:01:21 +0800

    先pull在push

--- src/cn/gimtom/web/controller/DoinbController.java
+++ src/cn/gimtom/web/controller/DoinbController.java
@@ -3,9 +3,11 @@ package cn.gimtom.web.controller;
 public class DoinbController {
     public static void main(String[] args) {
 
-        System.out.println("删除文件增加文件");
+
 
         System.out.println("先进行pull，在push");
+
+        System.out.println("先更新，在提交");
 
     }
 }
